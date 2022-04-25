6d9afbf245d9e61562a2fe7ced93a58ed0377501
exaream <emctwi8@gmail.com>
2022-04-25 19:18:21 +0900

    Update fileutil

--- fileutil/file.go
+++ fileutil/file.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// Exist returns true if a directory of a file exists.
 func Exist(target string) bool {
 	if _, err := os.Stat(target); err != nil {
 		return false
@@ -13,6 +14,14 @@ func Exist(target string) bool {
 	return true
 }
 
+// Ext returns an extension of a file.
 func Ext(filePath string) string {
 	return strings.ToLower(filepath.Ext(filePath))
+}
+
+// Stem returns a file's stem (a file name without the extension.) by a path
+func Stem(file string) string {
+	base := filepath.Base(file)
+	ext := filepath.Ext(base)
+	return strings.TrimRight(base, ext) // or base[:len(base)-len(ext)]
 }
